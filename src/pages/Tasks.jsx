b8b03c26ce0946b60135247c3bfdc9164e654184
Prachi-2211 <prachidasila94@gmail.com>
2026-08-09 17:01:01 +0530

    Update Tasks.jsx

--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Navbar } from "../components/Navbar";
+import { Navbar } from "../Components/Navbar";
 import { supabase } from "../services/supabase";
 import { useAuth } from "../context/AuthContext";
 
